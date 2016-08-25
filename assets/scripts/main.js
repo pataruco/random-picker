e4683e67e1eeba25d88d65e13ea782d5defb1e08
pataruco <pataruco@gmail.com>
2016-08-25 17:54:25 +0100

    Add JS-1 students

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   //Variables
   // var students = [`Stella`, `Michael`, `Mathew`, `Ella`, `Elishea`, `Kieran`, `Kart`, `Aaron`, `Monika`, `Jun`, `Lee`]; // fewd 31
-  let students = ['Cameron','Francesca','Guy','Hila','Jamie','Javier','Jo','Joseph','Keith','Lisa','Lucie','Martin','Nick','Rory','Tanny']; // fewd 33
+  //let students = ['Cameron','Francesca','Guy','Hila','Jamie','Javier','Jo','Joseph','Keith','Lisa','Lucie','Martin','Nick','Rory','Tanny']; // fewd 33
+
+  let students = ['Chima', 'Matt', 'Nini'] // js 1
   var $ready = $('#ready');
   var $go = $('#go');
 
