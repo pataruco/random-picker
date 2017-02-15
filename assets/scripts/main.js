1afc92ee0ec3ca60e6f414c3f15ac457d4ac596f
Guy Routledge <mail@guyroutledge.co.uk>
2017-02-15 17:51:24 +0000

    Random picker 2.0
    * Hide names during drum roll
    * Fix bug where last name is not picked
    * Remove comma after last name in the list

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
   //let students = ['Cameron','Francesca','Guy','Hila','Jamie','Javier','Jo','Joseph','Keith','Lisa','Lucie','Martin','Nick','Rory','Tanny']; // fewd 33
 
   // let students = ['Chima', 'Matt', 'Nini', 'Dario', 'Mercedes', 'Hanna', 'Julia', 'Ralph', 'Luke']; // js 1
-  let students = ['Victor', 'Charlie', 'Miguel', 'Gareth', 'Petya', 'Ella', 'Guillaume', 'Tim', 'Tom', 'Therese', 'George'];
+  // let students = ['Victor', 'Charlie', 'Miguel', 'Gareth', 'Petya', 'Ella', 'Guillaume', 'Tim', 'Tom', 'Therese', 'George'];
+  let students = ['Yaz', 'Sandra', 'Suzannah', 'Dilraj', 'Danai', 'Dan', 'Magda', 'Luice', 'Crispy', 'Natalia'].sort();
   var $ready = $('#ready');
   var $go = $('#go');
 
@@ -12,13 +13,16 @@ $(document).ready(function(){
   function renderStudents () {
     $ready.empty();
     for (let i = 0; i < students.length; i++) {
-      $ready.append(`<span>${students[i]}, </span>`);
+      $ready.append(`<span>${students[i]} </span>`);
     }
   }
 
   function renderStudent(className, innerText) {
     $go.empty();
     $go.append(`<h1 class="${className}">${innerText}</h1>`);
+	setTimeout( function(){
+		$ready.fadeTo( 500, 1 );
+	}, 3000);
   }
 
   function pickStudent (e) {
@@ -26,9 +30,10 @@ $(document).ready(function(){
     let studentToPick = students[Math.floor(Math.random() * students.length)];
     let studentToRemove = students.indexOf(studentToPick);
     let animation = `animated tada`;
+	$ready.css( 'opacity', 0 );
     students.splice(studentToRemove, 1);
     renderStudent(`waiting`, `...`);
-    if (students.length === 0) {
+    if (typeof studentToPick === 'undefined' ) {
       renderStudent(animation, `Let's go to the Pub `);
     } else {
       $('#drum')[0].play();
