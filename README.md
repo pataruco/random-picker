6c43add4997594ef634ab768509e84559943c90e
pataruco <pataruco@users.noreply.github.com>
2020-08-13 10:38:13 +0000

    Deploying to gh-pages from master @ 07be12702b52aed56c2240f1084a3a0f0d311824 🚀

---
+++ README.md
@@ -0,0 +1,41 @@
+![Build and Deploy](https://github.com/pataruco/random-picker/workflows/Build%20and%20Deploy/badge.svg)
+
+![ga_cog_large_red_rgb](https://cloud.githubusercontent.com/assets/40461/8183776/469f976e-1432-11e5-8199-6ac91363302b.png)
+
+# London FEWD
+
+### Random name picker for final presentation
+
+## How to install?
+
+`git clone git@github.com:pataruco/random-picker.git`
+
+## How to run it?
+
+- Go to folder `random-picker`
+  ```sh
+  cd random-picker
+  ```
+- Open `index.html` in a browswer
+
+  ```sh
+  open index.html
+  ```
+
+## Deploy
+
+- Create a tag with the following syntax
+
+  ```sh
+  git tag FEWD*
+  ```
+
+- Push release tag
+
+  ```sh
+  git push --tag
+  ```
+
+- Open in the browser **[pataruco.github.io/random-picker/](pataruco.github.io/random-picker/)**
+
+Made with :heart: by [Pedro](https://github.com/pataruco), [John](https://github.com/jadshead), and [James](https://github.com/jmsherry).
---
+++ assets/scripts/main.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let students = [
+    'Caitlin',
+    'Nick',
+    'Christopher',
+    'Dominique',
+    'Graham',
+    'Guillaume',
+    'Liam',
+    'Marco',
+    'Marzia',
+    'Samantha',
+    'Sara ',
+  ].sort();
+
+  const studentReadyList = document.getElementById('ready');
+  const nextButton = document.getElementById('next');
+  const goContainer = document.getElementById('go');
+  const drumPlayer = document.getElementById('drum');
+
+  const renderStudents = () => {
+    studentReadyList.innerHTML = '';
+    students.map((student) => {
+      const text = document.createTextNode(`${student}`);
+      const li = document.createElement('li');
+      const span = document.createElement('span');
+      span.appendChild(text);
+      li.appendChild(span);
+      studentReadyList.appendChild(li);
+    });
+  };
+
+  const renderStudent = (className, innerText) => {
+    goContainer.innerHTML = '';
+    const h1 = document.createElement('h1');
+    const text = document.createTextNode(innerText);
+    h1.classList = className;
+    h1.appendChild(text);
+    goContainer.appendChild(h1);
+    setTimeout(() => {
+      studentReadyList.style.opacity = 1;
+    }, 2000);
+  };
+
+  const pickStudent = (event) => {
+    event.preventDefault();
+    let studentToPick = students[Math.floor(Math.random() * students.length)];
+    let studentToRemove = students.indexOf(studentToPick);
+    let animation = `animated tada`;
+    studentReadyList.style.opacity = 0;
+    students.splice(studentToRemove, 1);
+    renderStudent(`waiting`, `...`);
+    if (typeof studentToPick === 'undefined') {
+      renderStudent(animation, `Let's go to the Pub `);
+    } else {
+      drumPlayer.play();
+      setTimeout(() => {
+        renderStudent(animation, studentToPick);
+      }, 2000);
+    }
+    renderStudents();
+  };
+
+  nextButton.addEventListener('click', pickStudent);
+  window.addEventListener('load', renderStudents);
+}); // end of document
